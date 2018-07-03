dd8a3a1a1935b2a0ac4908d6e3cc2f54a3661f49
Ethan Haynes <eghaynes17@gmail.com>
2018-07-03 14:33:09 -0500

    fixed tests. added color output

--- javascript/test/hasPair.test.js
+++ javascript/test/hasPair.test.js
@@ -1,19 +1,23 @@
 const hasPair = require('../src/hasPair')
 
 const arr = [-1,1,2,5,3,4,5]
+const reset = '\x1b[0m'
+const green = '\x1b[32m'
+const red = '\x1b[31m'
+const answer = bool => bool ? `${green} correct${reset}` : `${red} incorrect${reset}`
 
 console.log(
-  (ans => `${ans} should equal false: ${false === ans}`)(hasPair(arr, -1))
+  (ans => `${reset}${ans} should equal false: ${answer(false === ans)}`)(hasPair(arr, -1))
 )
 console.log(
-  (ans => `${ans} should equal true: ${true === ans}`)(hasPair(arr, 0))
+  (ans => `${reset}${ans} should equal true: ${answer(true === ans)}`)(hasPair(arr, 0))
 )
 console.log(
-  (ans => `${ans} should equal true: ${true === ans}`)(hasPair(arr, 8))
+  (ans => `${reset}${ans} should equal true: ${answer(true === ans)}`)(hasPair(arr, 8))
 )
 console.log(
-  (ans => `${ans} should equal true: ${true === ans}`)(hasPair(arr, 10))
+  (ans => `${reset}${ans} should equal true: ${answer(true === ans)}`)(hasPair(arr, 10))
 )
 console.log(
-  (ans => `${ans} should equal false: ${false === ans}`)(hasPair(arr, 11))
+  (ans => `${reset}${ans} should equal false: ${answer(false === ans)}`)(hasPair(arr, 11))
 )
