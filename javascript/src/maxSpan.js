2b6ebac989007cc43542d208f674c69a332a5b41
Ethan Haynes <eghaynes17@gmail.com>
2018-07-03 20:26:46 -0500

    added fix for breaking early

--- javascript/src/maxSpan.js
+++ javascript/src/maxSpan.js
@@ -3,19 +3,26 @@ const maxSpan = (arr) => {
   let last = arr.length-1
   let max = 1
   for (let i = 0; i < arr.length; i++) {
+    if (arr.length/2 < i) break;
+
     if (dict[arr[i]] === undefined) {
         dict[arr[i]] = i
     }
     if (dict[arr[last-i]] === undefined) {
         dict[arr[last-i]] = last-i
     }
-    if (dict[arr[i]] != i) {
-      max = dict[arr[i]] - i + 1
-      break;
-    }
     if (dict[arr[last-i]] != last-i) {
-      max = (last-i) - (dict[arr[last-i]]) + 1
-      break;
+      tempMax = Math.abs((last-i) - (dict[arr[last-i]])) + 1
+      if (max < tempMax) {
+        max = tempMax
+      }
+    }
+
+    if (dict[arr[i]] != i) {
+      tempMax = Math.abs(dict[arr[i]] - i) + 1
+      if (max < tempMax) {
+        max = tempMax
+      }
     }
   }
   return max
@@ -23,3 +30,4 @@ const maxSpan = (arr) => {
 console.log(maxSpan([1, 2, 1, 1, 3]))
 console.log(maxSpan([1, 4, 2, 1, 4, 1, 4]))
 console.log(maxSpan([1, 4, 2, 1, 4, 4, 4]))
+console.log(maxSpan([1,2,1,4,5,6,7,8,9,10,11]))
