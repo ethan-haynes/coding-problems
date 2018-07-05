18e41f18d2ec016cc734b6f7b62f6234fde24420
Ethan Haynes <eghaynes17@gmail.com>
2018-07-05 08:28:59 -0500

    cleaned/ refactored

--- javascript/src/linkedList/addTwoNumbers.js
+++ javascript/src/linkedList/addTwoNumbers.js
@@ -12,23 +12,18 @@
  */
  // O(n) runtime
 var addTwoNumbers = function(l1, l2) {
-    const list = []
-    const itr = (ll1, ll2, num) => {
-        let val = num + (ll1.val || 0) + (ll2.val || 0)
-        if (ll1.next || ll2.next) {
-            list.push(val%10)
-            return itr(ll1.next || 0, ll2.next || 0, Math.floor(val/10))
-        } else {
-            if (val === 0 || Math.ceil(Math.log10(val+1)) === 1) {
-              return val
-            } else {
-                list.push(val%10)
-                return Math.floor(val /10)
-            }
-        }
-    }
-    list.push(itr(l1, l2, 0, 1))
-    return list
+  const list = []
+  const itr = (ll1, ll2, num) => {
+    let val = num + (ll1.val || 0) + (ll2.val || 0)
+    if (ll1.next || ll2.next)
+        return list.push(val%10), itr(ll1.next || 0, ll2.next || 0, Math.floor(val/10))
+    else
+        if (val === 0 || Math.ceil(Math.log10(val+1)) === 1)
+          return val
+        else
+          return list.push(val%10), Math.floor(val/10)
+  }
+  return list.push(itr(l1, l2, 0)), list
 }
 
 // test setup
