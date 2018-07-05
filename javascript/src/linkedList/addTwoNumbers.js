f70ae88c84561045d64825a63f5b60f4e2322e86
Ethan Haynes <eghaynes17@gmail.com>
2018-07-05 08:20:00 -0500

    javascript can't handle large number addition; moved to in memory solution

--- javascript/src/linkedList/addTwoNumbers.js
+++ javascript/src/linkedList/addTwoNumbers.js
@@ -13,20 +13,17 @@
  // O(n) runtime
 var addTwoNumbers = function(l1, l2) {
     const list = []
-    const handleVal = ({ val }, multiple) => val ? val * multiple : 0
-    const itr = (ll1, ll2, num, multiple, ll) => {
-        let val = num + handleVal(ll1,multiple) + handleVal(ll2,multiple)
+    const itr = (ll1, ll2, num) => {
+        let val = num + (ll1.val || 0) + (ll2.val || 0)
         if (ll1.next || ll2.next) {
-            list.push(Math.floor(val/multiple)%10)
-            return itr(ll1.next || 0, ll2.next || 0, val, multiple*10)
+            list.push(val%10)
+            return itr(ll1.next || 0, ll2.next || 0, Math.floor(val/10))
         } else {
-            if (val === 0) {
-                return val
-            } else if (Math.ceil(Math.log10(val+1)) === Math.ceil(Math.log10(multiple+1))) {
-                return  Math.floor(val / multiple)
+            if (val === 0 || Math.ceil(Math.log10(val+1)) === 1) {
+              return val
             } else {
-                list.push(Math.floor(val/multiple)%10)
-                return Math.floor(val / (multiple*10))
+                list.push(val%10)
+                return Math.floor(val /10)
             }
         }
     }
@@ -40,11 +37,11 @@ function ListNode(val) {
    this.next = null
 }
 
-const input1 = new ListNode(3)
+let input1 = new ListNode(3)
 input1.next = new ListNode(4)
 input1.next.next = new ListNode(2)
 
-const input2 = new ListNode(4)
+let input2 = new ListNode(4)
 input2.next = new ListNode(6)
 input2.next.next = new ListNode(5)
 console.log(addTwoNumbers(input1, input2)) // [7,0,8]
