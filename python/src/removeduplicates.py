4eca2dcc9fc78b156a4d25510d890235ea63a552
Ethan Haynes <eghaynes17@gmail.com>
2018-07-17 17:58:17 -0500

    added more versions

--- python/src/removeduplicates.py
+++ python/src/removeduplicates.py
@@ -1,4 +1,4 @@
-def removeduplicates(s):
+def rd1(s):
     letters = set()
     out = ''
     for i in s:
@@ -8,5 +8,21 @@ def removeduplicates(s):
 
     return out
 
-print(removeduplicates('abcdef'))
-print(removeduplicates('abcdeeeef'))
+print(rd1('abcdef'))
+print(rd1('abcdeeeef'))
+
+def rd2(s):
+    return ''.join(list(set([ i for i in s])))
+
+print(rd2('abcdef'))
+print(rd2('abcdeeeef'))
+
+
+def rd3(s):
+    hash = {}
+    for i in range(len(s)):
+        if s[i] not in hash: hash[s[i]] = 1
+    return ''.join(list(hash))
+
+print(rd3('abcdef'))
+print(rd3('abcdeeeef'))
