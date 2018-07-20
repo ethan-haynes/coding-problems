054cd672d6a7f3012e634cbf56e29c97dca307d4
Ethan Haynes <eghaynes17@gmail.com>
2018-07-20 08:54:03 -0500

    added find node in circular linked list

---
+++ python/src/find_loop.py
@@ -0,0 +1,26 @@
+"""
+    TASK: find the node that is referenced twice in a circular linked list
+    NOTE: Recursive Solution
+"""
+def find_loop_start(head):
+    hash = {}
+    def __find_loop_inner(node):
+        if not node: return None
+        _id = id(node)
+        if _id in hash: return node
+        else:
+            hash[_id] = 1
+            return __find_loop_inner(node.next)
+    return __find_loop_inner(head)
+
+class LL:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+h = LL(0)
+h.next = LL(1)
+h.next.next = h
+
+node = find_loop_start(h)
+print(node.val if node else node)
