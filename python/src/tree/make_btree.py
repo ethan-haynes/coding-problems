2cde022b734043fd9c5292133a60f270974d1505
Ethan Haynes <eghaynes17@gmail.com>
2018-07-24 21:23:07 -0500

    make sorted binary tree from sorted array

---
+++ python/src/tree/make_btree.py
@@ -0,0 +1,22 @@
+import math
+class Tree:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def make_btree(arr):
+    if not len(arr): return None
+    mid = math.floor(len(arr)/2)
+    head = Tree(arr[mid])
+    head.left, head.right = make_btree(arr[:mid]), make_btree(arr[mid+1:])
+    return head
+
+def traverse(root):
+    if root:
+        traverse(root.left)
+        print(root.data)
+        traverse(root.right)
+
+t = make_btree([1,2,3,4,5,6,7,8,9])
+traverse(t)
