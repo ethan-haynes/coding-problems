13f729ad95d22e2f4be3d4f0340ea8ff30a87e76
Ethan Haynes <eghaynes17@gmail.com>
2018-07-26 21:44:25 -0500

    added function to print all paths that equal sum

--- python/src/tree/find_sum.py
+++ python/src/tree/find_sum.py
@@ -1,6 +1,8 @@
 from make_btree import Tree, make_btree, traverse
 t = make_btree([1,2,3,4,5,6,7]) # setup
 
+
+# find all the paths that sum to target
 def find_sum(tree, target):
     sums, path = [], []
     def walk(root, count=0, arr=[]):
@@ -14,6 +16,16 @@ def find_sum(tree, target):
         if sum(n) == target: sums.append(n)
     return sums
 
+# print all the paths that sum to target
+def print_sum(root, target, count=0, arr=[]):
+    if root:
+        arr.append(root.data)
+        for i in range(len(arr)):
+            if sum(arr[i:]) == target: print(arr[i:])
+        print_sum(root.left, target, count+1, arr.copy())
+        print_sum(root.right, target, count+1, arr.copy())
+
+
 print('target: {t} result: {r}'.format(t=1, r=find_sum(t,1)))
 print('target: {t} result: {r}'.format(t=2, r=find_sum(t,2)))
 print('target: {t} result: {r}'.format(t=3, r=find_sum(t,3)))
@@ -21,3 +33,11 @@ print('target: {t} result: {r}'.format(t=4, r=find_sum(t,4)))
 print('target: {t} result: {r}'.format(t=5, r=find_sum(t,5)))
 print('target: {t} result: {r}'.format(t=6, r=find_sum(t,6)))
 print('target: {t} result: {r}'.format(t=7, r=find_sum(t,7)))
+
+print_sum(t,1)
+print_sum(t,2)
+print_sum(t,3)
+print_sum(t,4)
+print_sum(t,5)
+print_sum(t,6)
+print_sum(t,7)
