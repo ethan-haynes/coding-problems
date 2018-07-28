76544f1cf8902208a60db6b3c489ad15ad8f4197
Ethan Haynes <eghaynes17@gmail.com>
2018-07-28 17:29:33 -0500

    refactored code

--- python/src/permutations.py
+++ python/src/permutations.py
@@ -1,10 +1,7 @@
-def permuations(s):
-    def reduce(s):
-        if len(s):
-            return [s] + (reduce(s[:-1]) or [])
-    def rotate(s, n):
-        if n > 0:
-            return reduce(s) + (rotate(s[-1:] + s[:-1], n-1) or [])
-    return rotate(s, len(s))
+def permuations(s, n=None):
+    if n == None: n = len(s)
+    trim = lambda s: len(s) and [s] + (trim(s[:-1]) or [])
+    if n > 0:
+        return trim(s) + (permuations(s[-1:] + s[:-1], n-1) or [])
 
-print(permuations('abc'))
+print(permuations(''))
