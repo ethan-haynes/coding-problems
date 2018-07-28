85e239adac82dcedb46d0a08778f94d83a87fea8
Ethan Haynes <eghaynes17@gmail.com>
2018-07-27 21:05:04 -0500

    removed unneeded condition

--- python/src/grid.py
+++ python/src/grid.py
@@ -5,12 +5,10 @@ def grid(g):
     def _g(grid, row=0, col=0):
         grid[row][col] = 1
         if grid[N-1][N-1] == 0:
-            if col < N-1 and row < N-1:
-                _g(deepcopy(grid), row+1, col), _g(deepcopy(grid), row, col+1)
-            elif col < N-1:
-                _g(deepcopy(grid), row, col+1)
-            else:
+            if row < N-1:
                 _g(deepcopy(grid), row+1, col)
+            if col < N-1:
+                _g(deepcopy(grid), row, col+1)
         else:
             l.append(grid)
     _g(deepcopy(g))
