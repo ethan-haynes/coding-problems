96911d2092f94a9a88defe97bac02da36043f7e3
Ethan Haynes <eghaynes17@gmail.com>
2018-07-29 06:42:01 -0500

    fixed logic

--- python/src/permutations.py
+++ python/src/permutations.py
@@ -1,7 +1,14 @@
 def permuations(s, n=None):
     if n == None: n = len(s)
-    trim = lambda s: len(s) and [s] + (trim(s[:-1]) or [])
+    rotate = lambda s, n: n > 0 and [s] + (rotate(s[:1] + s[2:] + s[1], n-1) or [])
     if n > 0:
-        return trim(s) + (permuations(s[-1:] + s[:-1], n-1) or [])
+        return (rotate(s, len(s)) or []) + (permuations(s[-1:] + s[:-1], n-1) or [])
 
-print(permuations(''))
+print(permuations('abc'))
+
+
+#
+# abc
+# acb
+# bac
+# bca
