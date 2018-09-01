fe8cbc7d6500d6af746df6b1fdc367338e1328a4
Ethan Haynes <eghaynes17@gmail.com>
2018-08-31 22:01:22 -0500

    added summary_ranges

---
+++ python/src/summary_ranges.py
@@ -0,0 +1,27 @@
+'''
+Input:  [0,1,2,4,5,7]
+Output: ["0->2","4->5","7"]
+Explanation: 0,1,2 form a continuous range; 4,5 form a continuous range.
+'''
+def summary_ranges(nums):
+    if len(nums) <= 1: return nums
+    out = []
+    r, smmry = None, ''
+    
+    def handle_num():
+        if str(r-1) == smmry: out.append(smmry)
+        else:
+            out.append(smmry + '->{num}'.format(num=str(r-1)))
+
+    for i, v in enumerate(nums):
+        if r == None: r, smmry = v + 1, str(v)
+        else: 
+            if r == v: r += 1
+            else:
+                handle_num()
+                r, smmry = v + 1, str(v)
+        
+        if i == len(nums) - 1: handle_num()
+    return out
+
+print(summary_ranges([0,1,2,4,5,7]))
