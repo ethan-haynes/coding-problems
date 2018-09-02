416f3fdaaddf5ccc94e1aba95ab56c0f5fb10fe2
Ethan Haynes <eghaynes17@gmail.com>
2018-09-02 08:50:23 -0500

    implemented word dictionary

--- python/src/word_dictionary.py
+++ python/src/word_dictionary.py
@@ -4,7 +4,7 @@ class WordDictionary:
         """
         Initialize your data structure here.
         """
-        
+        self.trie = {}        
 
     def addWord(self, word):
         """
@@ -12,7 +12,11 @@ class WordDictionary:
         :type word: str
         :rtype: void
         """
-        
+        next = self.trie
+        for char in word:
+            if char not in next:
+                next[char] = {}
+            next = next[char]
 
     def search(self, word):
         """
@@ -20,10 +24,34 @@ class WordDictionary:
         :type word: str
         :rtype: bool
         """
+        def deep_search(head, chars):
+            next = head
+            for i, char in enumerate(chars):
+                if char  == '.':
+                    if i == len(chars) - 1: return True
+                    for key in next.keys():
+                        if deep_search(next[key], chars[i+1:]): return True
+
+                    return False
+                else:
+                    if char in next:
+                        if i == len(chars) - 1: return True
+                        next = next[char]
+                    else: return False
+        return deep_search(self.trie, word)
+
         
-
-
 # Your WordDictionary object will be instantiated and called as such:
-# obj = WordDictionary()
-# obj.addWord(word)
-# param_2 = obj.search(word)
+obj = WordDictionary()
+obj.addWord('word')
+print(obj.search('word'))
+obj.addWord('cats')
+obj.addWord('cord')
+print(obj.search('cats'))
+print(obj.search('wor.'))
+print(obj.search('w.rd'))
+print(obj.search('cord'))
+print(obj.search('..rd'))
+obj.addWord('ball')
+obj.addWord('belt')
+print(obj.search('b.l.'))
