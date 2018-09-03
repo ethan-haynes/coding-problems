33af096391f8e5fffecb878371b8cc1ce2905d7b
Ethan Haynes <eghaynes17@gmail.com>
2018-09-03 11:43:50 -0500

    added memo version of permutations

--- python/src/permutations.py
+++ python/src/permutations.py
@@ -5,3 +5,22 @@ def permuations(s, n=None):
         return (rotate(s, len(s)) or []) + (permuations(s[-1:] + s[:-1], n-1) or [])
 
 print(permuations('abc'))
+
+
+
+'''
+abc -> acb -> bac -> bca -> cab -> cba
+'''
+def permutations2(s):
+    out = {}
+
+    def rotate(s):
+        if s not in out:
+            out[s] = 0
+            rotate(s[1:] + s[0]), rotate(s[0] + s[2:] + s[1])
+
+    rotate(s)
+
+    return out.keys()
+
+print(permutations2('abc'))
