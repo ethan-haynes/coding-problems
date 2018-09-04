14b9434408b7a9e9db9664c009bedc3e62d8fcab
Ethan Haynes <eghaynes17@gmail.com>
2018-09-04 09:36:38 -0500

    memoize subsets of set

--- python/src/subsets_of_set.py
+++ python/src/subsets_of_set.py
@@ -1,14 +1,16 @@
-def subsets_of_set(s, out=None):
-    if out == None: out = []
-    
-    if len(s):
-        if s not in out:
-            out.append(s)        
-            if 1 < len(s):
-                subsets_of_set(s[:-1], out)
-                subsets_of_set(s[1:] + [s[0]], out)
+def subsets_of_set(s, out=None, memo=None):
+    if out == None: out, memo = [], set()
+    l = len(s)
+    if l:
+        v = s[0] + str(l)
+        if v not in memo:
+            memo.add(v), out.append(s)
+            if 1 < l:
+                subsets_of_set(s[:-1], out, memo)
+                subsets_of_set(s[1:] + [s[0]], out, memo)
 
     return out
 
 print(subsets_of_set(['a','b','c']))
 print(subsets_of_set(['e','f','g','h']))
+print(subsets_of_set(['i','j','k','l','m','n','o']))
