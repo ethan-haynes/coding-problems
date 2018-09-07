0cff1ae71276d67165ef1b3b6332871a326271d4
Ethan Haynes <eghaynes17@gmail.com>
2018-09-06 20:16:28 -0500

    added memoization

--- python/src/largest_sum.py
+++ python/src/largest_sum.py
@@ -1,16 +1,23 @@
 def largest_sum(arr):
-    largest = { 'sum': None }
-
+    largest, memo = { 'sum': None }, {}
+    
     def _find(start):
         s = 0
         for i in range(start, len(arr)):
-            s += arr[i]
+            key = f'{start},{i}'
+            if key not in memo:
+                s += arr[i]
+                memo[key] = s
+                _find(start + 1)
+            else:
+                s = memo[key]
+
             if largest['sum'] == None or largest['sum'] < s:
                 largest['sum'] = s
-            _find(start + 1)
     
     _find(0)
     
     return largest['sum']
 
 print(largest_sum([2,-8,3,-2,4,-10]))
+print(largest_sum([2,-8,3,-2,4,-10,2,-8,3,-2,4,-10,2,-8,3,-2,4,-10,2,-8,3,-2,4,-10]))
