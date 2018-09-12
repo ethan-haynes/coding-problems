ff15eb619e81643a021c1cdf49d6b28f8274a83b
Ethan Haynes <eghaynes17@gmail.com>
2018-09-12 16:26:30 -0500

    added new rotate image and rotate uneven

--- python/src/rotate_image.py
+++ python/src/rotate_image.py
@@ -13,6 +13,25 @@ def rotate_image(m=[]):
         m[row] = m[row][length:]
     return m
 
+def rotate_image2(m):
+    if m:
+        for i in range(len(m) - 1, -1, -1):
+            for j in range(len(m))):
+                n = m[i].pop(0)
+                m[j].append(n)
+
+def rotate_image_uneven(m):
+    if m:
+        row, col = len(m), len(m[0])
+        for i in range(row - 1, -1, -1):
+            for j in range(col):
+                n = m[i].pop(0)
+                if len(m) <= j:
+                    m.append([])
+                m[j].append(n)
+                if len(m[i]) == 0:
+                    m.pop()
+
 print(rotate_image([
     [1,2,3],
     [4,5,6],
