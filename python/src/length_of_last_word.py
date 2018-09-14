8e48ea288c4d66627d971582b6e884511a446e7f
Ethan Haynes <eghaynes17@gmail.com>
2018-09-14 16:34:06 -0500

    implemented math.sqrt

--- python/src/length_of_last_word.py
+++ python/src/length_of_last_word.py
@@ -5,6 +5,13 @@ def length_of_last_word(s=''):
     """
     return len(s.split()[-1])
 
+def lolw2(s):
+    if s:
+        words = s.split()
+        while words:
+            if word: return len(word)
+    return 0
+
 print(length_of_last_word('hello world'))
 print(length_of_last_word('hello'))
 print(length_of_last_word('hi hello how are you'))
---
+++ python/src/sqrt.py
@@ -0,0 +1,9 @@
+import math
+
+def sqrt(x):
+    return x**.5
+
+print(sqrt(9) == math.sqrt(9))
+print(sqrt(8) == math.sqrt(8))
+print(sqrt(123) == math.sqrt(123))
+print(sqrt(1234567) == math.sqrt(1234567))
