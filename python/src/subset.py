00786707f2e62dc51762086e19f317ea00f6eba1
Ethan Haynes <eghaynes17@gmail.com>
2018-09-14 09:19:18 -0500

    adding subsets

--- python/src/subset.py
+++ python/src/subset.py
@@ -7,4 +7,18 @@ def subset(s, n, arr=[]):
         return subset([s[-1]] + s[:-1], n-1, arr + sub(s, len(s)))
     else: return arr
 
+
+def subset2(nums):
+    if not nums: return [[]]
+    out = []
+    
+    def sub(nums, p):
+        if nums:
+            sub(nums[1:], p + [nums[0]])
+            sub(nums[1:], p)
+        else: out.append(p)
+    sub(nums, [])
+    return out
+
 print(subset([1,2,3], 3))
+print(subset2([1,2,3]))
